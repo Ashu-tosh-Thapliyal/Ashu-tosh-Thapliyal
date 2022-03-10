cd327a7a74163dbc4dfb34939f07efefecab9cc2
Ashutosh Thapliyal <you@example.com>
2022-03-11 00:19:32 +0530

    sum of number

---
+++ c program.2.c
@@ -0,0 +1,24 @@
+// s = 1 + 3 + 5 + 7 + 9.....n term 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,a,i;
+    a=0;
+
+
+    {
+    	for(i=1;i<=n;i++)
+    	a=a+i*2-1;
+    	
+	}
+	printf("%d",n);
+	getch();
+
+ 
+
+
+
+}
+
+
