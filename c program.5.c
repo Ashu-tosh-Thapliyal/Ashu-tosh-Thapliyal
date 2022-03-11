1dbc569d26c01d47ee81f8cdc397a921e6e30356
Ashutosh Thapliyal <you@example.com>
2022-03-11 22:04:25 +0530

    c program.5

---
+++ c program.5.c
@@ -0,0 +1,17 @@
+//s=1+1/2+1/3+1/4....n term
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,a,i;
+	printf("Enter the term");
+	scanf("%d",n);
+	
+	for(i=1;i<=n;i++)
+	{
+		printf("%d",a);
+		
+	}
+getch();
+
+}
