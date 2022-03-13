f3eda8e0915fb4c4c3072c372ba2156b9d4f411a
Ashutosh Thapliyal <you@example.com>
2022-03-13 05:47:21 +0530

    Total Number of Factors

---
+++ Total Number of Factors.c
@@ -0,0 +1,24 @@
+// Total Number Of factors;
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,n,c;
+	c=0;
+	printf("Enter the Term");
+	scanf("%d",n);
+	
+	for(i=1;i<=n;i++);
+	{
+		if(n%i==0)
+		{
+			c++;
+		}
+		
+	}
+	
+getch();
+	
+	
+	
+}
