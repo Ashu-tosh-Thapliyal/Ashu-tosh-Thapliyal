37003f876959735c91f706d22a2fda0099c81673
Ashutosh Thapliyal <you@example.com>
2022-04-06 06:11:29 +0530

    Pyramid pattern

---
+++ Pyramid pattern.c
@@ -0,0 +1,28 @@
+        //     *
+       //     ***
+      //     *****
+     //     *******
+    //     *********
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,k;
+	
+	for(i=1;i<=5;i++)
+	{
+		for(k=4;k>=1;k--)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=2*i-1;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+getch();
+	
+}
+
